cfdffdd26d77c6d116783d50d4729d2d993fe63f
Tiger22462 <xutiger2001@gmail.com>
2023-09-02 13:04:59 +0700

    fix error handle and optimize useeffect

--- frontend/web-app/app/start/page.tsx
+++ frontend/web-app/app/start/page.tsx
@@ -2,63 +2,37 @@
 
 "use client";
 import { useState, useEffect } from "react";
-import { loadWeb3Address , loadSignupContract } from "../web3lib/web3_helper";
-
+import { loadWeb3Address,request_Account} from "../web3lib/web3_helper";
 
 const start = () => {
-	const [address, setAddress] = useState<string | null>(null);
-	//Load Dynamic account with useEffect
-	useEffect(() => {
-		async function request_Account() {
+  const [address, setAddress] = useState<string | null>(null);
+  //Load Dynamic account with useEffect
+  useEffect(() => {
+    request_Account()
+      .then((result) => {
+        setAddress(result);
+      })
+      .catch((error) => {
+        console.error("Error fetching Ethereum account:", error);
+      });
+  }, [])
 
-			//Check if connect to metamask or not?
-			if ((window as any).ethereum) {
-				//console.log("Requesting account")
-				try {
-					const address = await (window as any).ethereum.request({
-						method: "eth_requestAccounts"
-					});
-					//console.log(address[0])
-					setAddress(address[0])
-					//console.log((window as any).ethereum.isConnected())
-
-				}
-				catch {
-					console.log("Please connect to metamask!")
-				}
-				//event listen for account change
-				(window as any).ethereum.on("accountsChanged", (address: string[]) => {
-					      if (address.length > 0) {
-							//console.log(address[0])
-					        setAddress(address[0]);
-					      } else {
-							console.log("no accounts")
-					        setAddress(null);
-					      }
-					    });
-			}else {alert("Metamask is not installed!")}
-		}
-		request_Account()
-	});
-
-
-
-	const loadAddress = async () => {
+  const loadAddress = async () => {
 		const web3_address = loadWeb3Address();
-		setAddress((await web3_address as any).address);
-	};
+		setAddress(((web3_address) as any).address);
+  };
 
-	return (
-		<div>
-			<div className="Upper">
-				<h1> Start Page for Connect wallet</h1>
-				<button onClick={loadAddress}>Metamask Button</button>
-			</div>
-			<div className="Address">
-				<h3> Address : {address}</h3>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div className="Upper">
+        <h1> Start Page for Connect wallet</h1>
+        <button onClick={loadAddress}>Metamask Button</button>
+      </div>
+      <div className="Address">
+        <h3> Address : {address}</h3>
+      </div>
+    </div>
+  );
 };
 
 export default start;
--- frontend/web-app/app/web3lib/web3_helper.tsx
+++ frontend/web-app/app/web3lib/web3_helper.tsx
@@ -7,17 +7,28 @@ import { sign } from "crypto";
 ////////////////////////////////////////////////////////////////////
 //loadWeb3Address return user.address ex. 0x34752E0DeC3d55xxxxxxxx
 export const loadWeb3Address = async () => {
-  //Check if connect to metamask or not?
-  if ((window as any).ethereum) {
-    console.log("Requesting account");
-    try {
-      const address = await (window as any).ethereum.request({
-        method: "eth_requestAccounts",
-      });
-      return {address : address[0]}
-    } catch {
-      console.log("Please connect to metamask!");
+  // Check if connected to Metamask or not
+  try {
+    if ((window as any).ethereum) {
+      console.log("Requesting account");
+      try {
+        const address = await (window as any).ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        return { address: address[0] };
+      } catch (error) {
+        if ((error as any).code === 4001) {
+          // User rejected the connection
+          console.log("User rejected connection to Metamask.");
+        } else {
+          console.error("An error occurred while connecting to Metamask:", error);
+        }
+      }
+    } else {
+      console.log("Metamask not detected. Please install or enable Metamask.");
     }
+  } catch (err) {
+    console.error("An error occurred:", err);
   }
 };
 ////////////////////////////////////////////////////////////////////
@@ -46,29 +57,21 @@ export const loadSignupContract = async (address:any) => {
 
 //set contractdata to use methods
 
-export const loadSignupContract1 = async () => {
-  if ((window as any) .ethereum) {
-    await (window as any).ethereum.request({ method: 'eth_requestAccounts' });
-  }
-
-  const provider = new (ethers as any).providers.Web3Provider((window as any).ethereum);
-  
-  // Load ETH account
-  const signer = provider.getSigner();
-  const address = await signer.getAddress();
-
-  // Network ID
-  const networkId = (await provider.getNetwork()).chainId;
-
-  // Set contract data
-  const contractdata = new ethers.Contract(
-    (SignupContract.networks as any)[networkId].address,
-    SignupContract.abi,
-    signer
-  );
-
-  return { contractdata };
-};
 
 
 ////////////////////////////////////////////////////////////////////
+
+
+export const request_Account = () => {
+  return new Promise<string | null>((resolve) => {
+    // Event listener for account change
+    (window as any).ethereum.on("accountsChanged", (address: string[]) => {
+      if (address.length > 0) {
+        resolve(address[0]);
+      } else {
+        console.log("No accounts");
+        resolve(null);
+      }
+    });
+  });
+}
